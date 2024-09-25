69e2159de54759fa74ba9dec477219c250f0517a
NathanaelMo <monnier.nathanael29@gmail.com>
2024-09-25 16:01:25 +0200

    Mise en place du satellite

--- src/nicellipse/v1/Plan.java
+++ src/nicellipse/v1/Plan.java
@@ -6,11 +6,14 @@ import nicellipse.component.NiSpace;
 import javax.swing.*;
 import java.awt.*;
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Plan {
 	NiSpace space = new NiSpace("Exercice 1", new Dimension(600, 450));
-	Satellite satellite = new Satellite();
+	Satellite satellite = new Satellite(75);
+	Satellite satellite2 = new Satellite(150);
+	ArrayList<Satellite> list = new ArrayList<>();
 
 
 	void moveSatellite(Satellite satellite) {
@@ -46,14 +49,21 @@ public class Plan {
 
 	private void init(){
 		space.setBackground(Color.lightGray);
+		this.satellite2.setVitesse(2);
+		list.add(satellite);
+		list.add(satellite2);
 		space.add(satellite);
+		space.add(satellite2);
 	}
 
 	private void movement(){
 		while (true) {
-			this.moveSatellite(satellite);
-			if(satellite.getX() >= space.getWidth() - satellite.getWidth()){
-				satellite.reset();
+
+			for(int i = 0; i < list.size(); i++){
+				this.moveSatellite(list.get(i));
+				if(list.get(i).getX() >= space.getWidth() - list.get(i).getWidth()){
+					list.get(i).reset();
+				}
 			}
 		}
 	}
